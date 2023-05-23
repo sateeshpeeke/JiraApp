bbd4448d312c2716a0b7346e35658dc8def7fa6c
kalya <kalya@LAPTOP-ISMHNQM0>
2023-05-23 11:15:51 +0530

    Initial Commit.

---
+++ src/main/java/com/jira/JiraAppApplication.java
@@ -0,0 +1,13 @@
+package com.jira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiraAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiraAppApplication.class, args);
+	}
+
+}
